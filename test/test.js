2053080bfb220f4540fdb4864fe5f00e24bec540
Ingy döt Net <ingy@ingy.net>
2014-09-02 08:57:29 -0600

    Make Coffee/JS imports more explicit

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-Pig = require('../lib/Pig/Latin').Pig
+Pig = {}
+Pig.Latin = require('../lib/Pig/Latin').Pig.Latin
 
 console.log(new Pig.Latin('Pig stratin').convert())
 console.log(new Pig.Latin('I Like Apples').convert())

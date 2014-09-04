c833b3325ee7faa22828fb1d1bdad1dc82c4fcca
David Oswald <daoswald@gmail.com>
2014-09-04 17:42:13 -0600

    Formatting cleanup in test.php

--- test/test.php
+++ test/test.php
@@ -9,9 +9,7 @@ class Latin {
   public function convert() {
     
     $callback = function($word) {
-
       $lword = strtolower($word);
-
       if( ! preg_match( '/^([^aeiou]*)(.*)$/', $lword, $matches ) ) {
         throw new Exception('error');
       }
@@ -24,7 +22,6 @@ class Latin {
       }
 
       $pword = $matches[2] . $matches[1] . $ay;
-
       if( preg_match( '/^[A-Z]/', $word ) ) {
         $pword = strtoupper( substr( $pword, 0, 1 ) ) . substr( $pword, 1 );
       }
@@ -34,7 +31,6 @@ class Latin {
 
     $pig_latin_words
       = array_map( $callback, preg_split( '/\ +/', $this->english ) );
-
     return join( ' ', $pig_latin_words );
     
   }

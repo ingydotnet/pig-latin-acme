38f5b8862c54eecab06549fc9f8e9bc335cfa92b
David Oswald <daoswald@gmail.com>
2014-09-04 17:35:57 -0600

    PHP constructor accepts single param.

--- test/test.php
+++ test/test.php
@@ -4,20 +4,17 @@ class Latin {
 
   private $english;
 
-  
-  public function __construct() {
-    $this->english = func_get_arg( func_num_args() == 2 ? 1 : 0 );
-//    print $this->english;
-  }
+  public function __construct($eng) { $this->english = $eng; }
 
   public function convert() {
     
     $callback = function($word) {
+
       $lword = strtolower($word);
+
       if( ! preg_match('/^([^aeiou]*)(.*)$/', $lword, $matches ) ) {
         throw new Exception('error');
       }
-      $ay = '';
       
       if( strlen($matches[1]) == 0 ) {
         $ay = 'way';
@@ -25,22 +22,26 @@ class Latin {
       else {
         $ay = 'ay';
       }
+
       $pword = $matches[2] . $matches[1] . $ay;
+
       if( preg_match('/^[A-Z]/', $word ) ) {
         $pword = strtoupper(substr($pword,0,1)) . substr($pword, 1);
       }
+
       return $pword;
     };
 
     $pig_latin_words
       = array_map( $callback, preg_split('/\ +/', $this->english ) );
+
     return join(" ", $pig_latin_words );
     
   }
 
 }
 
-$bar = new Latin("english", "Pig latin");
+$bar = new Latin("Pig latin");
 echo $bar->convert(), "\n";
 $baz = new Latin("Stringy thingy");
 echo $baz->convert(), "\n";

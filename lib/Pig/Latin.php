445199848e6ceff1fceacb67235a9f746f4fbb8c
David Oswald <daoswald@gmail.com>
2014-09-05 01:01:09 -0600

    Use PHP namespaces, eg Pig\\Latin

--- lib/Pig/Latin.php
+++ lib/Pig/Latin.php
@@ -1,37 +1,41 @@
 <?php
 
-class PigLatin {
+namespace Pig {
 
-  private $english;
+  class Latin {
 
-  public function __construct($eng) { $this->english = $eng; }
+    private $english;
 
-  public function convert() {
+    public function __construct($eng) { $this->english = $eng; }
 
-    $callback = function($word) {
-      $lword = strtolower($word);
-      if( ! preg_match( '/^([^aeiou]*)(.*)$/', $lword, $matches ) ) {
-        throw new Exception('error');
-      }
+    public function convert() {
 
-      if( strlen($matches[1]) == 0 ) {
-        $ay = 'way';
-      }
-      else {
-        $ay = 'ay';
-      }
+      $callback = function($word) {
+        $lword = strtolower($word);
+        if( ! preg_match( '/^([^aeiou]*)(.*)$/', $lword, $matches ) ) {
+          throw new Exception('error');
+        }
 
-      $pword = $matches[2] . $matches[1] . $ay;
-      if( preg_match( '/^[A-Z]/', $word ) ) {
-        $pword = strtoupper( substr( $pword, 0, 1 ) ) . substr( $pword, 1 );
-      }
+        if( strlen($matches[1]) == 0 ) {
+          $ay = 'way';
+        }
+        else {
+          $ay = 'ay';
+        }
 
-      return $pword;
-    };
+        $pword = $matches[2] . $matches[1] . $ay;
+        if( preg_match( '/^[A-Z]/', $word ) ) {
+          $pword = strtoupper( substr( $pword, 0, 1 ) ) . substr( $pword, 1 );
+        }
 
-    $pig_latin_words
-      = array_map( $callback, preg_split( '/\ +/', $this->english ) );
-    return join( ' ', $pig_latin_words );
+        return $pword;
+      };
+
+      $pig_latin_words
+        = array_map( $callback, preg_split( '/\ +/', $this->english ) );
+      return join( ' ', $pig_latin_words );
+
+    }
 
   }
 
--- test/test.php
+++ test/test.php
@@ -3,13 +3,13 @@
 include_once 'lib/Pig/Latin.php';
 
 
-$fizz = new PigLatin('Pig latin');
+$fizz = new Pig\Latin('Pig latin');
 echo $fizz->convert(), "\n";
 
-$bang = new PigLatin('Stringy thingy');
+$bang = new Pig\Latin('Stringy thingy');
 echo $bang->convert(), "\n";
 
-$fizzbang = new PigLatin('I Like Apples');
+$fizzbang = new Pig\Latin('I Like Apples');
 echo $fizzbang->convert(), "\n";
 
 ?>

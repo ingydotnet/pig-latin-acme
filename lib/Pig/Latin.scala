d8e6477f4b489a9478d21a526071d8f7026ce4fb
Nami-Doc <vendethiel@hotmail.fr>
2014-09-09 22:51:45 +0200

    Create Latin.scala

---
+++ lib/Pig/Latin.scala
@@ -0,0 +1,20 @@
+package pig
+
+case class Latin(text: String) {
+	def convert: String = {
+		val pattern = """(?i)^([^aeiou]*)(.*)$"""
+		val Pattern = pattern.r
+
+		val words = for {
+			word <- text.split(" +")
+		} yield word.toLowerCase match {
+			case Pattern(cons, rest) =>
+				val ay = if (cons.isEmpty()) "way" else "ay"
+				val pword = rest + cons + ay
+				(if (word(0).isUpper) pword.capitalize else pword)
+			case _ => throw new IllegalArgumentException(s"Word '${word}' is invalid")
+		}
+		
+		words mkString " "
+	}
+}

2d19e4e6e75325818af421262e20ef51e157db91
Ingy döt Net <ingy@ingy.net>
2014-09-18 13:45:31 -0700

    Refactor scala to look more like cafe

--- lib/Pig/Latin.scala
+++ lib/Pig/Latin.scala
@@ -2,17 +2,22 @@ package Pig
 
 case class Latin(english: String) {
   def convert: String = {
-    val re = "^([^aeiou]*)(.*)$".r
+    val pigLatinWords = english.split(" +").map(word => {
+      val lword = word.toLowerCase
 
-    val pigLatinWords = for {
-      word <- english.split(" +")
-    } yield word.toLowerCase match {
-      case re(cons, rest) =>
-        val ay = if (cons.isEmpty()) "way" else "ay"
-        val pword = rest + cons + ay
-        (if (word(0).isUpper) pword.capitalize else pword)
-      case _ => throw new Exception("error")
-    }
+      val re = "^([^aeiou]*)(.*)$".r
+      val match_ = lword match {
+        case re(m1, m2) => List(m1, m2)
+        case _ => throw new Exception("error")
+      }
+
+      val ay = if (match_(0).isEmpty()) "way" else "ay"
+      val pword = match_(1) + match_(0) + ay
+      if (word(0).isUpper)
+        pword.capitalize
+      else
+        pword
+    })
     pigLatinWords mkString " "
   }
 }

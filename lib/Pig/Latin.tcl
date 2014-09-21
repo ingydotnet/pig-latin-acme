a616db404a21ccef051bfa3e00828fa9d278d287
Ingy döt Net <ingy@ingy.net>
2014-09-20 20:30:25 -0700

    Fixes courtesy of Jeff Hobbs.

--- lib/Pig/Latin.tcl
+++ lib/Pig/Latin.tcl
@@ -11,11 +11,12 @@ oo::class create Pig::Latin {
         set pig_latin_words {}
         foreach {word} [regexp -all -inline {\S+} $english] {
             set lword [string tolower $word]
-            regexp {^([^aeiou]*)(.*)$} $lword match match1 match2
-            # TODO die unless match
-            set ay [expr ("$match1"=="")?"way":"ay"]
+            if {![regexp {^([^aeiou]*)(.*)$} $lword match match1 match2]} {
+                error "Error"
+            }
+            set ay [expr {($match1=="")?"way":"ay"}]
             set pword $match2$match1$ay
-            if [regexp {^[A-Z]} $word] {
+            if {[regexp {^[A-Z]} $word]} {
                 lappend pig_latin_words [string toupper $pword 0 0]
             } else {
                 lappend pig_latin_words $pword
